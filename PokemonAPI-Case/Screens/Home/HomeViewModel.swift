72cbce6e0acd158e6b223b5de640ca4678c11b71
Gizem Duman <45661081+dumanngizem@users.noreply.github.com>
2023-03-21 23:49:15 +0300

    PAC-0006 add HomeView and configure view

--- PokemonAPI-Case/Screens/Home/HomeViewModel.swift
+++ PokemonAPI-Case/Screens/Home/HomeViewModel.swift
@@ -7,12 +7,87 @@
 
 import Foundation
 
-protocol HomeViewDataSource {}
+protocol HomeViewDataSource {
+    var numberOfRowsInSection: Int { get }
+    
+    func cellForRowAt(_ indexPath: IndexPath) -> PokemonCellProtocol?
+}
 
-protocol HomeViewEventSource {}
+protocol HomeViewEventSource {
+    var reloadData: VoidClosure? { get set }
+    var emptyViewState: BoolClosure? { get set }
+}
 
-protocol HomeViewProtocol: HomeViewDataSource, HomeViewEventSource {}
+protocol HomeViewProtocol: HomeViewDataSource, HomeViewEventSource {
+    func viewDidLoad()
+    func didSelectRowAt()
+}
 
 final class HomeViewModel: BaseViewModel<HomeRouter>, HomeViewProtocol {
     
+    private var cellItems: [PokemonCellProtocol]?
+    
+    var reloadData: VoidClosure?
+    var emptyViewState: BoolClosure?
+}
+
+// MARK: - LyfeCycle
+extension HomeViewModel {
+    
+    func viewDidLoad() {
+       fetchPokemon()
+    }
+}
+
+// MARK: - DataSources
+extension HomeViewModel {
+    
+    func cellForRowAt(_ indexPath: IndexPath) -> PokemonCellProtocol? {
+        return cellItems?[indexPath.row]
+    }
+    
+    var numberOfRowsInSection: Int {
+        return cellItems?.count ?? 0
+    }
+}
+
+// MARK: - ConfigureContents
+extension HomeViewModel {
+    
+    private func configureCellItems(_ data: [PokemonItem]?) {
+        guard let data = data else { return }
+        let items = data.enumerated().map { (index, element) in
+            return PokemonCellModel(index: index, title: element.name)
+        }
+        cellItems = items
+    }
+}
+
+// MARK: - Requests
+extension HomeViewModel {
+    
+    private func fetchPokemon() {
+        showLoading?()
+        let request = PokemonRequest()
+        dataProvider?.request(for: request, result: { [weak self] result in
+            guard let self = self else { return }
+            self.hideLoading?()
+            switch result {
+            case .success(let response):
+                self.configureCellItems(response.results)
+                self.emptyViewState?(response.results?.isEmpty ?? true)
+                self.reloadData?()
+            case .failure(let error):
+                print("Error")
+            }
+        })
+    }
+}
+
+// MARK: - Routes
+extension HomeViewModel {
+    
+    func didSelectRowAt() {
+        router.pushPokemonDetails()
+    }
 }
